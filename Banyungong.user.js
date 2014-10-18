a7660620ff7202134019b6ea7edae6cb655f9fe0
9尾雪狐 <gameclamp@gmail.com>
2014-10-18 19:09:59 +0800

    Create Banyungong.user.js

---
+++ Banyungong.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Banyungong
+// @namespace   http://
+// @include     http://banyungong.net/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+$('#ucHeader1_btnSearch[title="工分5以上 可进行搜索"]').removeAttr('disabled');
